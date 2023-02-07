5f91a06c456cc7e8cb94a7e45cb42d13537cde57
gaalih <galih@mail.unpas.ac.id>
2023-02-07 11:17:36 +0700

    using react native+tailwind+react navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <SafeAreaView>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working!!</Text>
+      <Text className="text-center bg-sky-400 text-white px-5 py-2 rounded-lg">
+        button
+      </Text>
     </View>
+    // </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
