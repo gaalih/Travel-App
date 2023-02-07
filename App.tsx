e8af0cf407f6c69fd8f3b79a6bcf64ee58e3c074
gaalih <galih@mail.unpas.ac.id>
2023-02-07 14:00:25 +0700

    create page home with icons and using react native animation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/screens/Home";
+import Discover from "./src/screens/Discover";
+import Profile from "./src/screens/Profile";
 
+type RootStackParamList = {
+  Home: undefined;
+  Discover: undefined;
+  Profile: { userId: string };
+  Feed: { sort: "latest" | "top" } | undefined;
+};
+
+// const Stack = createNativeStackNavigator();
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
   return (
-    // <SafeAreaView>
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working!!</Text>
-      <Text className="text-center bg-sky-400 text-white px-5 py-2 rounded-lg">
-        button
-      </Text>
-    </View>
-    // </SafeAreaView>
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={Home} />
+        <RootStack.Screen name="Discover" component={Discover} />
+        <RootStack.Screen
+          name="Profile"
+          component={Profile}
+          // initialParams={{ userId: user.id }}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,103 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as Animatable from "react-native-animatable";
+import { Fontisto } from "@expo/vector-icons";
 
-export default function Home() {
+const Home = () => {
+  const Navigation = useNavigation();
+  useLayoutEffect(() => {
+    Navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView
+      style={styles.container}
+      className="relative bg-slate-50 flex-1"
+    >
+      <View>
+        <Animatable.View
+          className="px-5 pt-7 flex flex-row gap-1"
+          animation="pulse"
+          easing="ease-in-out"
+        >
+          <View className="self-center">
+            <MaterialCommunityIcons name="compass" size={25} color="#65a30d" />
+          </View>
+          <Text className="text-lime-600 self-center font-semibold tracking-widest text-lg">
+            TravelApp
+          </Text>
+        </Animatable.View>
+        <View className="px-10 flex flex-col mt-2 gap-2">
+          <Animatable.Text
+            className="text-slate-700 self- text-left font-semibold tracking-widest text-lg"
+            animation="fadeInLeft"
+          >
+            Enjoy the trip with
+          </Animatable.Text>
+          <Animatable.Text
+            className="text-lime-600 text-left font-semibold tracking-widest text-3xl"
+            animation="fadeInLeft"
+          >
+            Good Moments
+          </Animatable.Text>
+          <Animatable.Text
+            className="text-slate-500 font-semibold text-xs tracking-tight text-justify"
+            animation="fadeInRight"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            voluptatibus ipsa deserunt, fuga quas eos ab commodi animi maiores
+          </Animatable.Text>
+        </View>
+      </View>
+      <Animatable.View
+        className="absolute -bottom-32 -right-16 rotate-12"
+        animation="fadeInUp"
+      >
+        <Fontisto
+          name="compass-alt"
+          size={500}
+          color="#65a30d"
+          style={{ opacity: 0.5 }}
+        />
+      </Animatable.View>
+      <View className="absolute z-50 self-center bottom-10 bg-transparent">
+        <TouchableOpacity
+          className="bg-transparent"
+          onPress={() => Navigation.navigate("Discover")}
+        >
+          <Animatable.View
+            className=" bg-teal-400 backdrop-blur-lg text-slate-800 p-7 rounded-full shadow-xl shadow-cyan-700"
+            animation="pulse"
+            easing="ease-in-out"
+            iterationCount={"infinite"}
+          >
+            <Text className="bg-transparent text-2xl font-extrabold text-slate-50">
+              GO
+            </Text>
+          </Animatable.View>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop:
+      StatusBar.currentHeight !== undefined
+        ? StatusBar.currentHeight + 10
+        : StatusBar.currentHeight,
+  },
+});
+export default Home;
